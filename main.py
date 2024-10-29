496324ad96a8c8f1a20f4f15b344e4c6b723c4be
illa1 <ilia.verstuk@gmail.com>
2024-10-29 19:28:20 +0200

    create_project

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, session, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'school.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#========== MODELS ====================================
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(25), nullable=False)
+    role = db.Column(db.Integer(), default=0, nullable=False)
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+#========== ROUTS =====================================
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
